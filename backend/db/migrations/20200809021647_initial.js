f1c0660e21202a898075e83cda5b8280462379e5
saifdev1642 <saifdev1642@gmail.com>
2020-08-09 04:27:46 +0600

    user table migration

--- backend/db/migrations/20200809021647_initial.js
+++ backend/db/migrations/20200809021647_initial.js
@@ -1,8 +1,16 @@
+const Knex = require('knex');
+const tableName = require('../../src/constants/tableName');
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+    await knex.schema.createTable(tableName.user,(table)=>{
+        table.increments().notNullable();
+        table.string('email',254).notNullable().unique();
+        table.string('name').notNullable();
+        table.string('password',500).notNullable();
+        table.dateTime('last_login');
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTable(tableName.user);
 };
